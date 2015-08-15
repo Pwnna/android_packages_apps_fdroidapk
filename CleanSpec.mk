5932981d15d7f150dcc38006324dac0015f9568f
Shuhao Wu <shuhao@shuhaowu.com>
2015-08-15 12:21:23 -0400

    CleanSpec and Android.mk updates
    Incremental build test 1?
    
    Change-Id: I08b3ebabcc228b5889ef20afad71a486b76191a1

---
+++ CleanSpec.mk
@@ -0,0 +1,4 @@
+$(call add-clean-step, rm -rf packages/apps/FDroid/F-Droid.apk)
+$(call add-clean-step, rm -rf packages/apps/FDroid/fdroid-last-modified)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/F-Droid_intermediates)
+
